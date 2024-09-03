af23100b7abb5f4983f64d772baf668739fe4f0a
ArifRahman <azrif27018@gmail.com>
2024-09-03 12:17:59 +0700

    blokdankomentar

---
+++ src/main/kotlin/blokdankomentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+    //Contoh komentar satu baris
+    /*
+    contoh komentar multi baris
+     */
+    /**
+     * kotlin Doc --> baris ini untuk mendokumentasi kode program
+     */
+}
